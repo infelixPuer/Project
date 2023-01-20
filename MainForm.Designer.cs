900b4b1d9f74aad2f1609ecc966ed5734e2f2653
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2023-01-20 15:51:46 +0100

    Add buttons and child forms

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pStatusBar = new System.Windows.Forms.Panel();
+            this.pButtons = new System.Windows.Forms.Panel();
+            this.pWindows = new System.Windows.Forms.Panel();
+            this.bTasks = new System.Windows.Forms.Button();
+            this.bTasksTypes = new System.Windows.Forms.Button();
+            this.bPleasantTasks = new System.Windows.Forms.Button();
+            this.bPleasantTasksShop = new System.Windows.Forms.Button();
+            this.pButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pStatusBar
+            // 
+            this.pStatusBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pStatusBar.Location = new System.Drawing.Point(0, 0);
+            this.pStatusBar.Name = "pStatusBar";
+            this.pStatusBar.Size = new System.Drawing.Size(800, 45);
+            this.pStatusBar.TabIndex = 0;
+            // 
+            // pButtons
+            // 
+            this.pButtons.Controls.Add(this.bPleasantTasksShop);
+            this.pButtons.Controls.Add(this.bPleasantTasks);
+            this.pButtons.Controls.Add(this.bTasksTypes);
+            this.pButtons.Controls.Add(this.bTasks);
+            this.pButtons.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pButtons.Location = new System.Drawing.Point(0, 45);
+            this.pButtons.Name = "pButtons";
+            this.pButtons.Size = new System.Drawing.Size(150, 405);
+            this.pButtons.TabIndex = 1;
+            // 
+            // pWindows
+            // 
+            this.pWindows.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pWindows.Location = new System.Drawing.Point(150, 45);
+            this.pWindows.Name = "pWindows";
+            this.pWindows.Size = new System.Drawing.Size(650, 405);
+            this.pWindows.TabIndex = 2;
+            // 
+            // bTasks
+            // 
+            this.bTasks.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bTasks.Location = new System.Drawing.Point(0, 0);
+            this.bTasks.Name = "bTasks";
+            this.bTasks.Size = new System.Drawing.Size(150, 45);
+            this.bTasks.TabIndex = 0;
+            this.bTasks.Text = "Tasks";
+            this.bTasks.UseVisualStyleBackColor = true;
+            this.bTasks.Click += new System.EventHandler(this.bTasks_Click);
+            // 
+            // bTasksTypes
+            // 
+            this.bTasksTypes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bTasksTypes.Location = new System.Drawing.Point(0, 45);
+            this.bTasksTypes.Name = "bTasksTypes";
+            this.bTasksTypes.Size = new System.Drawing.Size(150, 45);
+            this.bTasksTypes.TabIndex = 1;
+            this.bTasksTypes.Text = "Tasks types";
+            this.bTasksTypes.UseVisualStyleBackColor = true;
+            this.bTasksTypes.Click += new System.EventHandler(this.bTasksTypes_Click);
+            // 
+            // bPleasantTasks
+            // 
+            this.bPleasantTasks.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bPleasantTasks.Location = new System.Drawing.Point(0, 90);
+            this.bPleasantTasks.Name = "bPleasantTasks";
+            this.bPleasantTasks.Size = new System.Drawing.Size(150, 45);
+            this.bPleasantTasks.TabIndex = 2;
+            this.bPleasantTasks.Text = "Pleasant tasks";
+            this.bPleasantTasks.UseVisualStyleBackColor = true;
+            this.bPleasantTasks.Click += new System.EventHandler(this.bPleasantTasks_Click);
+            // 
+            // bPleasantTasksShop
+            // 
+            this.bPleasantTasksShop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bPleasantTasksShop.Location = new System.Drawing.Point(0, 135);
+            this.bPleasantTasksShop.Name = "bPleasantTasksShop";
+            this.bPleasantTasksShop.Size = new System.Drawing.Size(150, 45);
+            this.bPleasantTasksShop.TabIndex = 3;
+            this.bPleasantTasksShop.Text = "Pleasant tasks shop";
+            this.bPleasantTasksShop.UseVisualStyleBackColor = true;
+            this.bPleasantTasksShop.Click += new System.EventHandler(this.bPleasantTasksShop_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pWindows);
+            this.Controls.Add(this.pButtons);
+            this.Controls.Add(this.pStatusBar);
+            this.Name = "MainForm";
+            this.Text = "Task Manager";
+            this.pButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel pStatusBar;
+        private Panel pButtons;
+        private Panel pWindows;
+        private Button bPleasantTasksShop;
+        private Button bPleasantTasks;
+        private Button bTasksTypes;
+        private Button bTasks;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -1,10 +1,48 @@
-namespace Project
+namespace Project;
+
+public partial class MainForm : Form
 {
-    public partial class MainForm : Form
+    private Form _activeForm;
+
+    public MainForm()
     {
-        public MainForm()
+        InitializeComponent();
+    }
+
+    private void bTasks_Click(object sender, EventArgs e)
+    {
+        ActivateChildForm(new ChildForms.Tasks());
+    }
+
+    private void bTasksTypes_Click(object sender, EventArgs e)
+    {
+        ActivateChildForm(new ChildForms.TasksTypes());
+    }
+
+    private void bPleasantTasks_Click(object sender, EventArgs e)
+    {
+        ActivateChildForm(new ChildForms.PleasantTasks());
+    }
+
+    private void bPleasantTasksShop_Click(object sender, EventArgs e)
+    {
+        ActivateChildForm(new ChildForms.PleasantTasksShop());
+    }
+
+    private void ActivateChildForm(Form childForm)
+    {
+        if (_activeForm != null)
         {
-            InitializeComponent();
+            _activeForm.Activate();
         }
+
+        _activeForm = childForm;
+        childForm.TopLevel = false;
+        childForm.FormBorderStyle = FormBorderStyle.None;
+        childForm.Dock = DockStyle.Fill;
+        pWindows.Controls.Add(childForm);
+        pWindows.Tag = childForm;
+        childForm.BringToFront();
+        childForm.Show();
     }
 }
